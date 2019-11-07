284ce64d8905e371eaea3e19caedde9b508eaaf5
Leonard Crestez <leonard.crestez@nxp.com>
2019-11-07 18:36:34 +0200

    net: fec: Use fec_devinfo for s32v234
    commit 28d9ddb5629ac82014363a3942f610fbac929a40 from
    https://source.codeaurora.org/external/autobsps32/linux
    
    The change is necessary after b5da1152f7: "net: fec: set GPR
    bit on suspend by DT configuration."
    
    This commit was cherry-picked from
    git@bitbucket.sw.nxp.com/lfac/linux-nxp.git: 7efa749a4981
    "net: fec: Add initial s32v234 support" and modified
    according to the current BSP version.
    
    Issue: ALB-5843
    
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Reviewed-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -136,6 +136,13 @@ static const struct fec_devinfo fec_imx6ul_info = {
 		  FEC_QUIRK_HAS_COALESCE,
 };
 
+static const struct fec_devinfo fec_s32v234_info = {
+	.quirks = FEC_QUIRK_ENET_MAC | FEC_QUIRK_HAS_GBIT |
+		  FEC_QUIRK_HAS_BUFDESC_EX | FEC_QUIRK_HAS_CSUM |
+		  FEC_QUIRK_HAS_VLAN | FEC_QUIRK_HAS_AVB |
+		  FEC_QUIRK_ERR007885 | FEC_QUIRK_BUG_CAPTURE,
+};
+
 static struct platform_device_id fec_devtype[] = {
 	{
 		/* keep it for coldfire */
@@ -162,6 +169,9 @@ static struct platform_device_id fec_devtype[] = {
 	}, {
 		.name = "imx6ul-fec",
 		.driver_data = (kernel_ulong_t)&fec_imx6ul_info,
+	}, {
+		.name = "s32v234-fec",
+		.driver_data = (kernel_ulong_t)&fec_s32v234_info,
 	}, {
 		/* sentinel */
 	}
